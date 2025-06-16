58f885c756ccbe8ba9c813319b2983738909d246
Youngchan Kim <yckim@suhotech.com>
2025-06-16 19:02:53 +0900

    아이콘 추가. 버전 1.1.0.0

--- QuickLaunchOnTray/Properties/AssemblyInfo.cs
+++ QuickLaunchOnTray/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("QuickLaunchOnTray")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Use 'Quick Launch' on your system tray.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Youngchan Kim")]
 [assembly: AssemblyProduct("QuickLaunchOnTray")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright 2025. Youngchan Kim All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호가 자동으로 
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
